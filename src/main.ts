220fd301d3bf5562716d195268eb34440de82577
jkworth <jkworth@gmail.com>
2020-03-25 08:45:03 -0400

    added compression and moved to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import {NestFactory, Reflector} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {ClassSerializerInterceptor} from '@nestjs/common';
+import * as compression from 'compression';
+import {FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        new FastifyAdapter()
+    );
+    app.use(compression());
     app.setGlobalPrefix('api/v1');
     const reflector = app.get(Reflector);
     app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
-    await app.listen(3000);
+
+    await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap();
