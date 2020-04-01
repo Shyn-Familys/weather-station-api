41ddcfed24d2187eb5826cebb199597dec3d1cf5
jkworth <jkworth@gmail.com>
2020-04-01 16:27:17 -0400

    fix(querying): updated how querying and sorting is done
    use querybuilder to use query params
    use exclusive range when calling getSince endpoint

--- src/weewx/base.service.spec.ts
+++ src/weewx/base.service.spec.ts
@@ -7,7 +7,6 @@ import {Repository} from 'typeorm';
 import {ServiceBase} from './base.service';
 import {ArchiveBaseEntity} from './entities/archiveBase.entity';
 
-
 describe('Service Base', () => {
 
     @Injectable()
@@ -22,12 +21,20 @@ describe('Service Base', () => {
     process.env.STATION_TIMEZONE = 'UTC';
 
     beforeEach(async () => {
+
+        const repositoryMock = jest.fn(() => ({
+            createQueryBuilder: jest.fn(() => ({
+                where: jest.fn().mockReturnThis(),
+                orderBy: jest.fn().mockReturnThis(),
+            })),
+        }))();
+
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 ServiceBaseSpec,
                 {
                     provide: getRepositoryToken(ArchiveBaseEntity),
-                    useClass: Repository,
+                    useValue: repositoryMock,
                 }
             ]
         }).compile();
@@ -41,23 +48,20 @@ describe('Service Base', () => {
 
     describe('getToday', () => {
         it('should ask for only the results for today', async () => {
-            const expectedTime = moment().tz('UTC').startOf('day').unix();
             spyOn(nestjsTypeormPaginate, 'paginate').and.returnValue([]);
 
             await service.getToday({limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: expectedTime
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 
@@ -69,150 +73,129 @@ describe('Service Base', () => {
             await service.getSince(expectedTime, {limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: Math.floor(expectedTime / 1000)
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 
     describe('getLast24Hours', () => {
         it('should ask for only the results for the last 24 hours', async () => {
-            const expectedTime = moment().tz('UTC').subtract(1, 'day').unix();
             spyOn(nestjsTypeormPaginate, 'paginate').and.returnValue([]);
 
             await service.getLast24Hours({limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: expectedTime
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 
     describe('getYesterday', () => {
         it('should ask for only the results for yesterday', async () => {
-            const end = moment().tz('UTC').startOf('day');
-            const start = moment(end).subtract(1, 'days');
             spyOn(nestjsTypeormPaginate, 'paginate').and.returnValue([]);
 
             await service.getYesterday({limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: [start.unix(), end.unix()]
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 
     describe('getThisWeek', () => {
         it('should ask for only the results for this week', async () => {
-            const expectedTime = moment().tz('UTC').startOf('week').unix();
             spyOn(nestjsTypeormPaginate, 'paginate').and.returnValue([]);
 
             await service.getThisWeek({limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: expectedTime
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 
     describe('getThisMonth', () => {
         it('should ask for only the results for this month', async () => {
-            const expectedTime = moment().tz('UTC').startOf('month').unix();
             spyOn(nestjsTypeormPaginate, 'paginate').and.returnValue([]);
 
             await service.getThisMonth({limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: expectedTime
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 
     describe('getThisQuarter', () => {
         it('should ask for only the results for this quarter', async () => {
-            const expectedTime = moment().tz('UTC').startOf('quarter').unix();
             spyOn(nestjsTypeormPaginate, 'paginate').and.returnValue([]);
 
             await service.getThisQuarter({limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: expectedTime
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 
     describe('getThisYear', () => {
         it('should ask for only the results for this year', async () => {
-            const expectedTime = moment().tz('UTC').startOf('year').unix();
             spyOn(nestjsTypeormPaginate, 'paginate').and.returnValue([]);
 
             await service.getThisYear({limit: 10, page: 1, route: 'testing/route'});
 
             expect(nestjsTypeormPaginate.paginate).toHaveBeenCalledWith(
-                expect.any(Object),
-                expect.any(Object),
                 expect.objectContaining({
-                    order: expect.objectContaining({dateTime: 'ASC'}),
-                    where: expect.objectContaining({
-                        dateTime: expect.objectContaining({
-                            _value: expectedTime
-                        })
-                    })
+                    where: expect.any(Function),
+                    orderBy: expect.any(Function)
+                }),
+                expect.objectContaining({
+                    limit: 10, page: 1, route: 'testing/route'
                 })
             );
+            expect(nestjsTypeormPaginate.paginate).toHaveBeenCalled();
         });
     });
 });
--- src/weewx/base.service.ts
+++ src/weewx/base.service.ts
@@ -1,6 +1,7 @@
 import * as moment from 'moment-timezone';
 import {IPaginationOptions, paginate, Pagination} from 'nestjs-typeorm-paginate';
-import {Between, FindConditions, MoreThanOrEqual, Repository} from 'typeorm';
+import {Repository} from 'typeorm';
+import {SelectQueryBuilder} from 'typeorm/query-builder/SelectQueryBuilder';
 import {ArchiveBaseEntity} from './entities/archiveBase.entity';
 
 export abstract class ServiceBase<Entity extends ArchiveBaseEntity> {
@@ -10,57 +11,65 @@ export abstract class ServiceBase<Entity extends ArchiveBaseEntity> {
     ) {
     }
 
-    private static getRange(from: number, to?: number): FindConditions<object> {
-        return {
-            where: {
-                dateTime: to ? Between(from, to) : MoreThanOrEqual(from)
-            },
-            order: {
-                dateTime: 'ASC'
-            }
-        };
-    }
-
     async getToday(options: IPaginationOptions): Promise<Pagination<Entity>> {
-        const query = ServiceBase.getRange(moment().tz(process.env.STATION_TIMEZONE).startOf('day').unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getRangeQuery(moment().tz(process.env.STATION_TIMEZONE).startOf('day').unix());
+        return await paginate(query, options);
     }
 
     async getSince(startTime: number, options: IPaginationOptions): Promise<Pagination<Entity>> {
-        const query = ServiceBase.getRange(moment(startTime).tz(process.env.STATION_TIMEZONE).unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getSinceQuery(moment(startTime).tz(process.env.STATION_TIMEZONE).unix());
+        return await paginate(query, options);
     }
 
     async getLast24Hours(options: IPaginationOptions): Promise<Pagination<Entity>> {
-        const query = ServiceBase.getRange(moment().tz(process.env.STATION_TIMEZONE).subtract(1, 'day').unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getRangeQuery(moment().tz(process.env.STATION_TIMEZONE).subtract(1, 'day').unix());
+        return await paginate(query, options);
     }
 
     async getYesterday(options: IPaginationOptions): Promise<Pagination<Entity>> {
         const endDate = moment().tz(process.env.STATION_TIMEZONE).startOf('day');
         const startDate = moment(endDate).subtract(1, 'days');
 
-        const query = ServiceBase.getRange(startDate.unix(), endDate.unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getRangeQuery(startDate.unix(), endDate.unix());
+        return await paginate(query, options);
     }
 
     async getThisWeek(options: IPaginationOptions): Promise<Pagination<Entity>> {
-        const query = ServiceBase.getRange(moment().tz(process.env.STATION_TIMEZONE).startOf('week').unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getRangeQuery(moment().tz(process.env.STATION_TIMEZONE).startOf('week').unix());
+        return await paginate(query, options);
     }
 
     async getThisMonth(options: IPaginationOptions): Promise<Pagination<Entity>> {
-        const query = ServiceBase.getRange(moment().tz(process.env.STATION_TIMEZONE).startOf('month').unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getRangeQuery(moment().tz(process.env.STATION_TIMEZONE).startOf('month').unix());
+        return await paginate(query, options);
     }
 
     async getThisQuarter(options: IPaginationOptions): Promise<Pagination<Entity>> {
-        const query = ServiceBase.getRange(moment().tz(process.env.STATION_TIMEZONE).startOf('quarter').unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getRangeQuery(moment().tz(process.env.STATION_TIMEZONE).startOf('quarter').unix());
+        return await paginate(query, options);
     }
 
     async getThisYear(options: IPaginationOptions): Promise<Pagination<Entity>> {
-        const query = ServiceBase.getRange(moment().tz(process.env.STATION_TIMEZONE).startOf('year').unix());
-        return await paginate(this.repository, options, query);
+        const query = this.getRangeQuery(moment().tz(process.env.STATION_TIMEZONE).startOf('year').unix());
+        return await paginate(query, options);
+    }
+
+    private getSinceQuery(from: number): SelectQueryBuilder<Entity> {
+        const query = this.repository.createQueryBuilder('a');
+        query.where('a.dateTime > :from', {from});
+        query.orderBy('a.dateTime', 'ASC');
+
+        return query;
+    }
+
+    private getRangeQuery(from: number, to?: number): SelectQueryBuilder<Entity> {
+        const query = this.repository.createQueryBuilder('a');
+        if (to) {
+            query.where('a.dateTime between :from and :to', {from, to});
+        } else {
+            query.where('a.dateTime >= :from', {from});
+        }
+
+        return query.orderBy('dateTime', 'ASC');
     }
 }
